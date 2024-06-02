65701669eb65102d670390d017444e3bd0470306
MoranB@DBMaestro.com <MoranB@DBMaestro.com>
2024-06-02 10:46:35 +0300

    Fixed column

--- dbo/Tables/Creation-Scripts/Emp.sql
+++ dbo/Tables/Creation-Scripts/Emp.sql
@@ -4,6 +4,6 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Emp](
 	[FirstName] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[LastName1] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+	[LastName] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 GO
