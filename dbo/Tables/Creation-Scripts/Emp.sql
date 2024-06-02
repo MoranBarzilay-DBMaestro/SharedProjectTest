2a702c7393523f45ad44796f856e4662f61d445f
MoranB@DBMaestro.com <MoranB@DBMaestro.com>
2024-06-02 10:38:33 +0300

    New Emp table

---
+++ dbo/Tables/Creation-Scripts/Emp.sql
@@ -0,0 +1,9 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Emp](
+	[FirstName] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[LastName1] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+)
+GO
